b79070d91d4630cc340a7f3f6ddc255d01b5085c
bagatir <bakhmudov01@mail.ru>
2023-11-21 13:56:55 +0300

    modified 'usually_keyboard.py'

--- keyboards/usually_keyboard.py
+++ keyboards/usually_keyboard.py
@@ -3,7 +3,8 @@ from aiogram import types
 
 def group_keyboard(group_list):
     kb = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    for group in group_list:
-        btn = types.KeyboardButton(group[0])
-        kb.add(btn)
+
+    for i in range(0, len(group_list), 3):
+        row_buttons = group_list[i:i + 3]
+        kb.row(*[types.KeyboardButton(group[0]) for group in row_buttons])
     return kb
